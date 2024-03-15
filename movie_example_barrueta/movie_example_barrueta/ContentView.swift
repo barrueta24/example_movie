ec37b7b1301183a70eb0643d86eb801deb166a86
barrueta24 <127541778+barrueta24@users.noreply.github.com>
2024-03-14 18:45:09 -0600

    project moview Barrueta

---
+++ movie_example_barrueta/movie_example_barrueta/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  movie_example_barrueta
+//
+//  Created by Jonathan hugo Barrueta Aguilar on 14/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ movie_example_barrueta/movie_example_barrueta/movie_example_barruetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_example_barruetaApp.swift
+//  movie_example_barrueta
+//
+//  Created by Jonathan hugo Barrueta Aguilar on 14/03/24.
+//
+
+import SwiftUI
+
+@main
+struct movie_example_barruetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
